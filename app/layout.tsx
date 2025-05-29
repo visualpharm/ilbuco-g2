5b45b6aee19899a57f7b0eec33c8d1e9872608e9
Ivan Braun <ivan@visualpharm.com>
2025-05-28 23:36:57 -0300

    refactor: update globals.css import path to use alias syntax

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import "./globals.css"
+import "@/styles/globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { LanguageProvider } from "@/contexts/language-context"
 import { ClientLayout } from '@/components/client-layout'
