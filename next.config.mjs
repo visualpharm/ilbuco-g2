728111ec2ed6b02c917291354819e6c5bae11223
Ivan Braun <ivan@visualpharm.com>
2025-07-01 15:48:12 -0300

    redirects

--- next.config.mjs
+++ next.config.mjs
@@ -16,6 +16,16 @@ const nextConfig = {
         destination: '/contacts',
         permanent: true,
       },
+      {
+        source: '/que-hacer-en-carilo-en-invierno',
+        destination: '/que-hacer-en-carilo/en-invierno',
+        permanent: true,
+      },
+      {
+        source: '/que-hacer-en-carilo-cuando-llueve',
+        destination: '/que-hacer-en-carilo/cuando-llueve',
+        permanent: true,
+      },
     ]
   },
 }
