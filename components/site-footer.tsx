22383ab5beb5be108e4931a686e6801f6d65dc0b
Ivan Braun <ivan@icons8.com>
2026-09-06 23:02:28 -0300

    fix: use inglesconjenny.com for Jenny links

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -118,7 +118,7 @@ export function SiteFooter() {
               <a href="https://usabruno.com">Bruno</a>
               <a href="https://lira-voice.app">Lira</a>
               <a href="https://humanrounds.org">Human Rounds</a>
-              <a href="https://inglesconjenny.vercel.app">Inglés con Jenny</a>
+              <a href="https://inglesconjenny.com">Inglés con Jenny</a>
               <a href="https://finda-capital.com">Finda Capital</a>
             </nav>
           )}
